94aa7ff1d7507974dcb252ae90020512131a66ee
rohimoz28 <rohimoz28@gmail.com>
2020-12-28 16:29:57 +0700

    tambah menu baru sukses

---
+++ application/views/menu/index.php
@@ -0,0 +1,71 @@
+<!-- Begin Page Content -->
+<div class="container-fluid">
+
+	<!-- Page Heading -->
+	<h1 class="h3 mb-4 text-gray-800"><?= $title ?></h1>
+
+	<div class="div row">
+		<div class="col-lg-6">
+
+			<?= form_error('menu', '<div class="alert alert-danger" role="alert">', '</div>'); ?>
+
+			<!-- Notifikasi muncul jika data berhasil di tambahkan -->
+			<?= $this->session->flashdata('message'); ?>
+
+			<a href="" class="btn btn-primary mb-3" data-toggle="modal" data-target="#exampleModal">Add New Menu</a>
+
+			<table class="table table-hover">
+				<thead>
+					<tr>
+						<th scope="col">#</th>
+						<th scope="col">Menu</th>
+						<th scope="col">Action</th>
+					</tr>
+				</thead>
+				<tbody>
+					<?php $i = 1; ?>
+					<?php foreach ($menu as $m) : ?>
+						<tr>
+							<th scope="row"><?= $i; ?></th>
+							<td><?= $m['menu']; ?></td>
+							<td>
+								<a href="" class="badge badge-pill badge-success">Edit</a>
+								<a href="" class="badge badge-pill badge-danger">Delete</a>
+							</td>
+						</tr>
+						<?php $i++; ?>
+					<?php endforeach; ?>
+				</tbody>
+			</table>
+
+		</div>
+	</div>
+
+</div>
+<!-- /.container-fluid -->
+
+</div>
+<!-- End of Main Content -->
+
+<!-- Modal -->
+<div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+	<div class="modal-dialog">
+		<div class="modal-content">
+			<div class="modal-header">
+				<h5 class="modal-title" id="exampleModalLabel">Add New Menu</h5>
+				<button type="button" class="close" data-dismiss="modal" aria-label="Close">
+					<span aria-hidden="true">&times;</span>
+				</button>
+			</div>
+			<form action="menu" method="POST">
+				<div class="modal-body">
+					<input type="text" class="form-control" id="menu" name="menu" placeholder="Menu name">
+				</div>
+				<div class="modal-footer">
+					<button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+					<button type="submit" class="btn btn-primary">Save changes</button>
+				</div>
+			</form>
+		</div>
+	</div>
+</div>
